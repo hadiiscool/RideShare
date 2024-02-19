dd7092ed0859872bd54e5e3191a2bbdef0fe56a4
Hadi A <hammar24@students.polytechnic.org>
2024-02-19 12:50:57 -0800

    tester not working, talk to rosato

---
+++ RideShareTester.java
@@ -0,0 +1,14 @@
+public class RideShareTester {
+    public static void main (String [] args){
+    
+    Road T1 = new Road(10,5, 5);
+    System.out.println(T1.toString());
+    System.out.println();
+    
+    System.out.println("updating...");
+    System.out.println();
+    
+    T1.run();
+    System.out.println(T1.toString());
+    }
+}
