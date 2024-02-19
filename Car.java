f53c8569f1696d4e65dbed23756754d73cd14a38
Hadi A <hammar24@students.polytechnic.org>
2024-02-18 18:25:03 -0800

    1 mothed

--- Car.java
+++ Car.java
@@ -1,3 +1,44 @@
+import java.util.ArrayList;
 public class Car {
     
+
+
+    //fields
+    private static int idGen;
+    private int idNum;
+    private int location;
+    private int destinaton;
+    private double balance;
+    private boolean isMovingleft;
+    private boolean isMovingright;
+    private ArrayList<Passenger> people; 
+    
+    //constructors
+    public Car(int myLocation, int myDestinaton){
+        people = new ArrayList<Passenger>();
+        location = myLocation;
+        destinaton = myDestinaton;
+        idNum = idGen;
+        idGen++;
+        
+        balance = 0.0;
+        if(destinaton < location){
+            isMovingleft = true;
+            isMovingright = false;
+        }
+        else{
+            isMovingleft = false;
+            isMovingright = true;
+        }
+    }
+
+    //methods
+
+    //getters
+    public ArrayList<Passenger> getpassangerlist(){
+        return people;//retnrs this array list, still need to add ppl to it
+    }
+
 }
+
+   
