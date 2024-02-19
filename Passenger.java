469cd1e04ef4629a143f5fcd5b39a3aa55b6648b
Hadi A <hammar24@students.polytechnic.org>
2024-02-18 18:19:59 -0800

    passengert basics

--- Passenger.java
+++ Passenger.java
@@ -1,3 +1,56 @@
 public class Passenger {
-    
+    //feilds
+    private int start;
+    private int dest;
+    private static int idGen;
+    private int idNum;
+    private boolean isInCar;
+    private boolean isAtDest;
+
+
+
+//construcrtor
+public Passenger(int myStart, int myDest){
+    start = myStart;
+    dest = myDest;
+    isInCar = false;
+    idNum = idGen;
+    idGen++;
+    isAtDest = false;
+}
+
+//methods
+
+public int getStartPos(){
+    return start;
+}
+
+public int getDestPos(){
+    return dest;
+}
+
+public boolean getIsInCar(){
+    return isInCar;
+}
+
+public void putInCar(){
+    isInCar = true;
+}
+
+public void removefromCar(){
+    isInCar = false;
+}
+
+public int getIdNum(){
+    return idNum;
+}
+
+public void AtDest(){
+    isAtDest = true;
+}
+
+public String toString(){
+    return "Passenger ID: " + idNum + " Passenger Destination: " + dest+ " Are they at their Destination? " + isAtDest;
+}
+
 }
